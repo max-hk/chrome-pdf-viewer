cb5e7babbd75c4295fbcf4840b684896bf72ce10
maxloh <35831069+maxloh@users.noreply.github.com>
2019-05-23 22:56:08 +0800

    Clean up code

--- chrome-pdf-reader.go
+++ chrome-pdf-reader.go
@@ -1,14 +1,16 @@
 // go build -ldflags "-H windowsgui" chrome-pdf-reader.go
 package main
 
-import "os"
-import "os/exec"
-import "syscall"
-import "net/url"
-import "strings"
+import (
+    "os"
+    "os/exec"
+    "syscall"
+    "net/url"
+    "strings"
+)
 
 func main() {
-	cmd := exec.Command("cmd", "/C", "C:\\Program Files (x86)\\Google\\Chrome Dev\\Application\\chrome.exe", "--app=" + strings.ReplaceAll(url.PathEscape(os.Args[1]), "%5C", "\\"))
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	cmd.Run()
+    cmd := exec.Command("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", "--app=" + strings.ReplaceAll(url.PathEscape(os.Args[1]), "%5C", "\\"))
+    cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+    cmd.Run()
 }
