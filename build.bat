b7f6e4590af3e33899e95f48ccaada3fd28ed34e
maxloh <35831069+maxloh@users.noreply.github.com>
2019-05-24 23:29:49 +0800

    Run commands one by one

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-go build -ldflags "-H windowsgui" chrome-pdf-reader.go
-magick -background none -density 10000 pdf-box.svg -trim -size 256x256 -gravity center -scale 256x256 -extent 256x256 pdf-box.png
-magick convert pdf-box.png pdf-box.ico
+go build -ldflags "-H windowsgui" chrome-pdf-reader.go && ^
+magick -background none -density 10000 pdf-box.svg -trim -size 256x256 -gravity center -scale 256x256 -extent 256x256 pdf-box.png && ^
+magick convert pdf-box.png pdf-box.ico && ^
 ./rcedit-x64.exe chrome-pdf-reader.exe --set-icon  pdf-box.ico
