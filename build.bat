14a81e0db84df9a8bb8216c484586dbcbaf93e80
maxloh <35831069+maxloh@users.noreply.github.com>
2019-05-24 23:09:04 +0800

    Fix icon generated is blurry

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 go build -ldflags "-H windowsgui" chrome-pdf-reader.go
-magick convert -background none -size 256x256 pdf-box.svg -trim -gravity center -scale 256x256 -extent 256x256 pdf-box.png
+magick -background none -density 10000 pdf-box.svg -trim -size 256x256 -gravity center -scale 256x256 -extent 256x256 pdf-box.png
 magick convert pdf-box.png pdf-box.ico
 ./rcedit-x64.exe chrome-pdf-reader.exe --set-icon  pdf-box.ico
