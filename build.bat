53922934b65993a408703f435612c624617ab357
maxloh <35831069+maxloh@users.noreply.github.com>
2019-05-26 00:27:19 +0800

    Fix command not working & clean files after build

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 go build -ldflags "-H windowsgui" chrome-pdf-reader.go && ^
 magick -background none -density 10000 pdf-box.svg -trim -size 256x256 -gravity center -scale 256x256 -extent 256x256 pdf-box.png && ^
-magick convert pdf-box.png pdf-box.ico && ^
-./rcedit-x64.exe chrome-pdf-reader.exe --set-icon  pdf-box.ico
+magick pdf-box.png -define icon:auto-resize="255" pdf-box.ico && ^
+rcedit-x64.exe chrome-pdf-reader.exe --set-icon  pdf-box.ico
+del pdf-box.png
+del pdf-box.ico
+pause
